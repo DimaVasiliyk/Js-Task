aff4de0657eb0475d3c033381e24e994f3a39296
kaktuz777 <killer_kaktuz777@ukr.net>
2021-06-27 17:51:55 +0300

    21. Write a JavaScript program to flatten a nested (any depth) array. If you pass shallow, the array will only be flattened a single level.

---
+++ 21.js
@@ -0,0 +1,15 @@
+// 21. Write a JavaScript program to flatten a nested (any depth) array. If you pass shallow, the array will only be flattened a single level.  
+// Sample Data :
+// console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+// [1, 2, 3, 4, 5, 6]
+// console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+// [1, 2, 3, [[4]], 5, 6]
+
+//
+
+func = (array, isDeepOne = false) =>{
+return array.flat(isDeepOne ? 1 : Infinity)
+}
+
+const test =  [1, [2], [3, [[4]]],[5,6]];
+console.log(func(test));
