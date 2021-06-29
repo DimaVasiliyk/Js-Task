98395a0cf0fe4b893de35e2ef19abcfd72bcce1f
kaktuz777 <killer_kaktuz777@ukr.net>
2021-06-29 17:01:43 +0300

    31. Write a JavaScript function to remove a specific element from an array

---
+++ 31.js
@@ -0,0 +1,18 @@
+// 31. Write a JavaScript function to remove a specific element from an array 
+// Test data :
+// console.log(remove_array_element([2, 5, 9, 6], 5));
+// [2, 9, 6]
+function remove(array, n){
+   let index = array.indexOf(n);
+   if (index === -1){
+       return ('не найдено')
+   } else {
+    (index > -1)
+    array.splice(index, 1);
+    }
+   return array;
+}
+
+console.log(remove([2, 5, 9, 6],5));
+
+
